2a50e5f3b8e35f3172edd19a35b746a65106994d
newb1er <32424677+newb1er@users.noreply.github.com>
2022-04-20 14:42:40 +0800

    ✨ set up algo structure

---
+++ src/algo/GVI.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class GVI:
+    def start(self, **kwargs) -> int:
+        """ start value iteration
+
+        Args:
+            **kwargs: hyperparameters (or add other arguments as you wish)
+
+        Returns:
+            int: number of iterations
+        """
+
+        # TODO: remove this line and write your code
+        raise NotImplementedError
