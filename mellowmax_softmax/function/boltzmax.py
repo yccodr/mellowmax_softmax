fce330cd6a5df82e099d9896be236d6e911c2822
Fishbone Huang <eric56421@gmail.com>
2022-04-25 23:46:00 +0800

    [Update] Reduce boltzmax funtion into one.

--- mellowmax_softmax/function/boltzmax.py
+++ mellowmax_softmax/function/boltzmax.py
@@ -1,23 +1,21 @@
-from plum import dispatch
+from typing import Union
+
 import torch
 import numpy as np
+from torch import Tensor
+from numpy import ndarray
 
 
-@dispatch
-def boltzmax(x: torch.Tensor, beta=1) -> torch.Tensor:
-    x_exp = torch.exp(beta * x)
+def boltzmax(x: Union[ndarray, Tensor], beta=1) -> Union[ndarray, Tensor]:
+    if isinstance(x, ndarray):
+        x_exp = np.exp(beta * x)
 
-    s = torch.inner(x, x_exp)
-    s /= x_exp.sum()
+        s = np.inner(x, x_exp)
+        s /= np.sum(x_exp)
+    elif isinstance(x, Tensor):
+        x_exp = torch.exp(beta * x)
 
-    return s
-
-
-@dispatch
-def boltzmax(x: np.ndarray, beta=1) -> np.ndarray:
-    x_exp = np.exp(beta * x)
-
-    s = np.inner(x, x_exp)
-    s /= np.sum(x_exp)
+        s = torch.inner(x, x_exp)
+        s /= x_exp.sum()    
 
     return s
