7eb66d8ea2fae630b4de410caffb641759c2e0ef
Fishbone Huang <eric56421@gmail.com>
2022-04-25 23:16:39 +0800

    [Update] Implement boltzmax funciton.

--- mellowmax_softmax/function/boltzmax.py
+++ mellowmax_softmax/function/boltzmax.py
@@ -1,13 +1,23 @@
 from plum import dispatch
-from numpy import ndarray
-from torch import Tensor
+import torch
+import numpy as np
 
 
 @dispatch
-def boltzmax(x: Tensor) -> Tensor:
-    raise NotImplementedError
+def boltzmax(x: torch.Tensor, beta=1) -> torch.Tensor:
+    x_exp = torch.exp(beta * x)
+
+    s = torch.inner(x, x_exp)
+    s /= x_exp.sum()
+
+    return s
 
 
 @dispatch
-def boltzmax(x: ndarray) -> ndarray:
-    raise NotImplementedError
+def boltzmax(x: np.ndarray, beta=1) -> np.ndarray:
+    x_exp = np.exp(beta * x)
+
+    s = np.inner(x, x_exp)
+    s /= np.sum(x_exp)
+
+    return s
