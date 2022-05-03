0839364748f71b9cf14f421b235979785f23c825
Fishbone Huang <eric56421@gmail.com>
2022-05-03 14:20:53 +0800

    [Update] Add shift to exp to avoid overflow.

--- mellowmax_softmax/function/boltzmax.py
+++ mellowmax_softmax/function/boltzmax.py
@@ -8,12 +8,14 @@ from numpy import ndarray
 
 def boltzmax(x: Union[ndarray, Tensor], beta=1) -> Union[ndarray, Tensor]:
     if isinstance(x, ndarray):
-        x_exp = np.exp(beta * x)
+        c = x.max()
+        x_exp = np.exp(beta * (x - c))
 
         s = np.inner(x, x_exp)
         s /= np.sum(x_exp)
     elif isinstance(x, Tensor):
-        x_exp = torch.exp(beta * x)
+        c = x.max()
+        x_exp = torch.exp(beta * (x - c))
 
         s = torch.inner(x, x_exp)
         s /= x_exp.sum()
