96cc04ef2f376ad1db35354988ea7ef4b244baa2
newb1er <32424677+newb1er@users.noreply.github.com>
2022-05-03 21:55:39 +0800

    hotfix: boltzmax

--- mellowmax_softmax/function/boltzmax.py
+++ mellowmax_softmax/function/boltzmax.py
@@ -7,24 +7,25 @@ from numpy import ndarray
 
 
 class Boltzmax():
+
     def __init__(self, beta=1) -> None:
         self.beta = beta
-    
-    def __call__(self, x: Union[ndarray, Tensor], beta=1) -> Union[ndarray, Tensor]:
+
+    def __call__(self, x: Union[ndarray, Tensor]) -> Union[ndarray, Tensor]:
         if isinstance(x, ndarray):
             c = x.max()
-            x_exp = np.exp(beta * (x - c))
+            x_exp = np.exp(self.beta * (x - c))
 
             s = np.inner(x, x_exp)
             s /= np.sum(x_exp)
 
         elif isinstance(x, Tensor):
             c = x.max()
-            x_exp = torch.exp(beta * (x - c))
+            x_exp = torch.exp(self.beta * (x - c))
 
             s = torch.inner(x, x_exp)
             s /= x_exp.sum()
-            
+
         else:
             raise TypeError("x must be either np.ndarray or torch.Tensor")
 
