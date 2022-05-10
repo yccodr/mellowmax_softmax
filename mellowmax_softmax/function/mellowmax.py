b6532362fd5e0788f4eb734950f72bb5eaf8e012
Fishbone Huang <46958462+eric56421@users.noreply.github.com>
2022-05-10 17:44:42 +0800

    mellowmax Class (#14)
    * [Update] Add shift to exp to avoid overflow.
    
    * [Feat] Convert it into Class.

--- mellowmax_softmax/function/mellowmax.py
+++ mellowmax_softmax/function/mellowmax.py
@@ -1,28 +1,39 @@
-from plum import dispatch
-import torch
-from torch import Tensor
+from typing import Union
+
 import numpy as np
+import torch
 
 
-@dispatch
-def mellowmax(x: Tensor, omega=1) -> Tensor:
-    x = x * omega
-    x = torch.exp(x)
+class Mellowmax():
+    def __init__(self, omega=1) -> None:
+        self.omega = omega
 
-    s = torch.sum(x)
-    s /= torch.numel(x)
-    s = torch.log(s)
+    def __call__(self, x: Union[np.ndarray, torch.Tensor]) -> Union[np.ndarray, torch.Tensor]:
+        if isinstance(x, np.ndarray):
+            ## Shifter
+            c = x.max()
+            x = x - c
 
-    return s / omega
+            x = x * self.omega
+            x = np.exp(x)
 
+            s = np.sum(x, keepdims=True)
+            s /= np.size(x)
+            s = np.log(s)
 
-@dispatch
-def mellowmax(x: np.ndarray, omega=1) -> np.ndarray:
-    x = x * omega
-    x = np.exp(x)
+        elif isinstance(x, torch.Tensor):
+            ## Shifter
+            c = x.max()
+            x = x - c
 
-    s = np.sum(x, keepdims=True)
-    s /= np.size(x)
-    s = np.log(s)
+            x = x * self.omega
+            x = torch.exp(x)
 
-    return s / omega
+            s = torch.sum(x)
+            s /= torch.numel(x)
+            s = torch.log(s)
+
+        else:
+            raise TypeError("x must be either np.ndarray or torch.Tensor")
+
+        return s / self.omega + c
