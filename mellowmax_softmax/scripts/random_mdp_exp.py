7463546c879416e7e98ff8b1586a91b0bb11485c
newb1er <32424677+newb1er@users.noreply.github.com>
2022-05-10 20:44:57 +0800

    feat & chore: count fixed points and clean up code

--- mellowmax_softmax/scripts/random_mdp_exp.py
+++ mellowmax_softmax/scripts/random_mdp_exp.py
@@ -1,13 +1,36 @@
+import json
+
 import gym
-from numpy import random
+import numpy as np
 from mellowmax_softmax.algo.GVI import GVI
 from mellowmax_softmax.function import boltzmax, mellowmax
+from numpy import fix, random
 from tqdm import tqdm
-import json
+
+
+class GVIExp:
+    ENV_NAME = ''
+    EXP_NAME = 'random_mdp_boltz'
+    FUNCTION_PARAM = 16.55
+    SEED = 7414
+    ITERATION = 100
+
+    def __init__(self):
+        random.seed = self.SEED
+
+    def make_env(self):
+        self.env = gym.make(self.ENV_NAME)
+
+    def dump_result(self):
+        # store result
+        with open(f'./exp_results/{self.EXP_NAME}_log.json', 'w') as f:
+            json.dump(result, f)
+
 
 random.seed = 7414
 ITERATION = 100
 EXP_NAME = 'random_mdp_boltz'
+BETA = 16.55
 
 result = {
     'num': ITERATION,
@@ -17,84 +40,55 @@ result = {
 }
 
 
-def boltz_test_termination():
+def test_termination(softmax):
     for round in range(200):
         env = gym.make('RandomMDP-v0')
-
         gvi = GVI()
-        gvi.beta = 16.55
 
         avg_iter = 0
         no_term = 0
         multi_fixed_point_cnt = 0
+        fixed_points = []
 
-        for _ in tqdm(range(ITERATION)):
+        for seed in tqdm(range(ITERATION)):
             gvi.reset()
             gvi.set_env(env)
-            gvi.set_softmax(boltzmax.boltzmax)
+            gvi.set_softmax(softmax)
 
             # NOTE: delta not specified
             gvi.set_delta(1e-3)
             gvi.set_max_iter(1000)
+            gvi.rng.seed(seed)
+
+            q_max = []
 
             if not gvi.start():
                 no_term += 1
             else:
                 avg_iter += gvi.get_result()
 
-        result['avg_iter'] += avg_iter
+            for s in range(gvi.num_states):
+                idx = np.argmax(gvi.Q[s])
+                q_max.append(np.unravel_index(idx, gvi.Q[s].shape))
 
         avg_iter /= (ITERATION - no_term)
 
-        print(
-            f'round: {round}, no terminate: {no_term}, avg. iteration: {avg_iter}'
-        )
-
-        if no_term > 0:
-            result['no_term'] += 1
-
-        if multi_fixed_point_cnt > 0:
-            result['multi_fixed_point'] += 1
-
-        result['avg_iter'] += avg_iter
-
-    result['avg_iter'] /= (ITERATION * 200 - result['no_term'])
-
-
-def mellow_test_termination():
-    for round in range(200):
-        env = gym.make('RandomMDP-v0')
-        gvi = GVI()
-        gvi.beta = 16.55
-
-        avg_iter = 0
-        no_term = 0
-        multi_fixed_point_cnt = 0
-
-        for _ in tqdm(range(ITERATION)):
-            gvi.reset()
-            gvi.set_env(env)
-            gvi.set_softmax(mellowmax.mellowmax)
-
-            # NOTE: delta not specified
-            gvi.set_delta(1e-3)
-            gvi.set_max_iter(1000)
-
-            if not gvi.start():
-                no_term += 1
-            else:
-                avg_iter += gvi.get_result()
-
-        avg_iter /= (ITERATION - no_term)
+        new_fix_point = False
+        for fixed_point in fixed_points:
+            if fixed_point == q_max:
+                new_fix_point = True
+                break
+        if len(fixed_points) == 0 or new_fix_point:
+            fixed_points.append(q_max)
 
         print(
-            f'round: {round}, no terminate: {no_term}, avg. iteration: {avg_iter}'
+            f'round: {round}, no terminate: {no_term}, fixed_points: {len(fixed_points)}, avg. iteration: {avg_iter}'
         )
 
 
 exp = {
-    'random_mdp_boltz': boltz_test_termination,
-    'random_mdp_mellow': mellow_test_termination
+    'random_mdp_boltz': test_termination(boltzmax.Boltzmax(16.55)),
+    'random_mdp_mellow': test_termination(mellowmax.Mellowmax(16.55))
 }
 
 exp['random_mdp_boltz']()
