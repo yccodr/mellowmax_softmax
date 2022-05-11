53d0897249561ef7122091a22ab9a98dc9d569fb
00100000 <d10531318@gapps.fg.tp.edu.tw>
2022-05-11 17:59:28 +0800

    use policy, not operator

--- mellowmax_softmax/scripts/lunarLander.py
+++ mellowmax_softmax/scripts/lunarLander.py
@@ -2,21 +2,22 @@ import torch
 import gym
 from mellowmax_softmax.algo.reinforce import reinforce
 from mellowmax_softmax.model.lunarLanderPolicy import policy
-from mellowmax_softmax.function.boltzmax import boltzmax
-from mellowmax_softmax.function.mellowmax import mellowmax
+from mellowmax_softmax.function.boltzmax import BoltzmannPolicy
+from mellowmax_softmax.function.mellowmax import MellowmaxPolicy
+from mellowmax_softmax.function.eps_greedy import EpsGreedy
 
 random_seed = 20
 torch.manual_seed(random_seed)
 
 env = gym.make('LunarLander-v2')
-env.seed(random_seed)
+env.reset(seed=random_seed)
 
 discrete = isinstance(env.action_space, gym.spaces.Discrete)
 observation_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n if discrete else env.action_space.shape[0]
 hidden_size = 128
 
-policyNet = policy(action_dim, observation_dim, hidden_size)
+policyNet = policy(observation_dim, action_dim, hidden_size)
 
-PG = reinforce(env, policyNet, boltzmax, maxEpisodeNum=10)
+PG = reinforce(env, policyNet, BoltzmannPolicy(16.55), maxEpisodeNum=10)
 PG.train()
